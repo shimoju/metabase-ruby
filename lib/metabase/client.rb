edbbc12104da1c7fc3fbd1874d8223117a218531
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-05-12 23:46:15 +0900

    Add User-Agant

--- lib/metabase/client.rb
+++ lib/metabase/client.rb
@@ -10,7 +10,10 @@ module Metabase
     def initialize(url:, username:, password:)
       @username = username
       @password = password
-      @connection = Faraday.new(url: url) do |c|
+      headers = {
+        user_agent: "MetabaseRuby/#{VERSION} (#{RUBY_ENGINE}#{RUBY_VERSION})"
+      }
+      @connection = Faraday.new(url: url, headers: headers) do |c|
         c.request :json
         c.response :json, content_type: /\bjson$/
         c.adapter Faraday.default_adapter
