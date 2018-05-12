c223cafb78d86e041a0a4b23de69a7f0012802cc
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-05-12 19:24:51 +0900

    Add vcr and webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 require 'bundler/setup'
 require 'metabase'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/vcr_cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+end
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = '.rspec_status'
