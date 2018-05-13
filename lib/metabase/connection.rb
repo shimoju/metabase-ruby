dd58978fa192e4e374ec039908bc7542e6831582
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-05-13 10:24:34 +0900

    fix rubocop

--- lib/metabase/connection.rb
+++ lib/metabase/connection.rb
@@ -9,7 +9,8 @@ module Metabase
         c.request :json
         c.response :json, content_type: /\bjson$/
         c.adapter Faraday.default_adapter
-        c.headers['User-Agent'] = "MetabaseRuby/#{VERSION} (#{RUBY_ENGINE}#{RUBY_VERSION})"
+        c.headers['User-Agent'] =
+          "MetabaseRuby/#{VERSION} (#{RUBY_ENGINE}#{RUBY_VERSION})"
       end
     end
   end
