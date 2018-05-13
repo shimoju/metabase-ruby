12ab3d30cfc409333bc2e8c76d88a4b953f06d7d
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-05-13 19:35:32 +0900

    Add common request method

--- lib/metabase/connection.rb
+++ lib/metabase/connection.rb
@@ -6,7 +6,37 @@ require 'metabase/error'
 
 module Metabase
   module Connection
+    def get(path, params = {})
+      request(:get, path, params)
+    end
+
+    def post(path, params = {})
+      request(:post, path, params)
+    end
+
+    def put(path, params = {})
+      request(:put, path, params)
+    end
+
+    def delete(path, params = {})
+      request(:delete, path, params)
+    end
+
+    def head(path, params = {})
+      request(:head, path, params)
+    end
+
     private
+
+    def request(method, path, params)
+      response = connection.public_send(method, path, params) do |request|
+        request.headers['X-Metabase-Session'] = @token if @token
+      end
+
+      error = Error.from_response(response)
+      raise error if error
+      response.body
+    end
 
     def connection
       @connection ||= Faraday.new(url: @url) do |c|
