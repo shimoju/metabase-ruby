39824e21ee8c499885b09d7752aa9049313a8c01
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-05-13 11:38:56 +0900

    Add frozen_string_literal: true

--- lib/metabase/error.rb
+++ lib/metabase/error.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Metabase
   class Error < StandardError
     def self.from_response(response)
--- spec/metabase/endpoint/session_spec.rb
+++ spec/metabase/endpoint/session_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe Metabase::Endpoint::Session do
   let(:client) do
     Metabase::Client.new(
