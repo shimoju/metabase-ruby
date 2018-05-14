ee6bdbb1d62f15ac0c2f9d8b21b2e306c70ee999
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-05-14 23:22:04 +0900

    Add error classes

--- lib/metabase/error.rb
+++ lib/metabase/error.rb
@@ -6,6 +6,14 @@ module Metabase
       klass =
         case response.status
         when 400 then BadRequest
+        when 401 then Unauthorized
+        when 403 then Forbidden
+        when 404 then NotFound
+        when 400..499 then ClientError
+        when 500 then InternalServerError
+        when 502 then BadGateway
+        when 503 then ServiceUnavailable
+        when 500..599 then ServerError
         end
       klass&.new(response)
     end
@@ -21,5 +29,14 @@ module Metabase
     end
   end
 
+  class ClientError < Error; end
   class BadRequest < Error; end
+  class Unauthorized < Error; end
+  class Forbidden < Error; end
+  class NotFound < Error; end
+
+  class ServerError < Error; end
+  class InternalServerError < Error; end
+  class BadGateway < Error; end
+  class ServiceUnavailable < Error; end
 end
