bed0929ac078716e4e4be84d1dec446ed6713ccd
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-05-15 08:41:47 +0900

    Move VCR config to spec/support

---
+++ spec/support/vcr.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/vcr_cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+end
