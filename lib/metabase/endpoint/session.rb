ba1028ee09f793100d2b6953dc13aa953f69dbfd
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-05-19 10:08:21 +0900

    Receive login info as args

--- lib/metabase/endpoint/session.rb
+++ lib/metabase/endpoint/session.rb
@@ -3,8 +3,8 @@
 module Metabase
   module Endpoint
     module Session
-      def login
-        params = { username: @username, password: @password }
+      def login(params = {})
+        params = { username: @username, password: @password }.merge(params)
         response = post('/api/session', params)
         @token = response['id']
       end
