3f1dc4a43d647ab8fa58b8facb0e07194864ae97
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-05-20 21:47:52 +0900

    Add Metabase::Endpoint::Async

---
+++ spec/metabase/endpoint/async_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+RSpec.describe Metabase::Endpoint::Async do
+  include_context 'login'
+
+  describe 'running_jobs', vcr: true do
+    context 'success' do
+      it 'returns all running jobs' do
+        running_jobs = client.running_jobs
+        expect(running_jobs).to be_kind_of(Array)
+      end
+    end
+  end
+end
