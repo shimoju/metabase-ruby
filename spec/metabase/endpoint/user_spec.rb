9902a207eed54f6b9c5df10699b3821541ba8a47
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-05-20 16:07:54 +0900

    Add #users

--- spec/metabase/endpoint/user_spec.rb
+++ spec/metabase/endpoint/user_spec.rb
@@ -3,6 +3,15 @@
 RSpec.describe Metabase::Endpoint::User do
   include_context 'login'
 
+  describe 'users', vcr: true do
+    context 'success' do
+      it 'returns all users' do
+        users = client.users
+        expect(users).to be_kind_of(Array)
+      end
+    end
+  end
+
   describe 'current_user', vcr: true do
     context 'success' do
       it 'returns the current user' do
