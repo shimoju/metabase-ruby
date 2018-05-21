46e32bc4b837be8572ccea0a65ab507b6dd55e48
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-05-21 22:56:02 +0900

    Add doc comment for #login

--- lib/metabase/endpoint/session.rb
+++ lib/metabase/endpoint/session.rb
@@ -3,6 +3,11 @@
 module Metabase
   module Endpoint
     module Session
+      # Login to Metabase.
+      #
+      # @param params [Hash] Request body
+      # @return [String] Session token
+      # @see https://github.com/metabase/metabase/blob/master/docs/api-documentation.md#post-apisession
       def login(params = {})
         params = { username: @username, password: @password }.merge(params)
         response = post('/api/session', params)
