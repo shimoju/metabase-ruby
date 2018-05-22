595cc378d53714e30837db3d1d13a5b8b687be79
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-05-22 23:09:06 +0900

    Add #logout

--- spec/metabase/endpoint/session_spec.rb
+++ spec/metabase/endpoint/session_spec.rb
@@ -3,15 +3,15 @@
 RSpec.describe Metabase::Endpoint::Session do
   include_context 'client'
 
-  let(:incorrect_password) do
-    Metabase::Client.new(
-      url: 'http://localhost:3030',
-      username: 'mb@example.com',
-      password: 'incorrect'
-    )
-  end
-
   describe 'login', vcr: true do
+    let(:incorrect_password) do
+      Metabase::Client.new(
+        url: 'http://localhost:3030',
+        username: 'mb@example.com',
+        password: 'incorrect'
+      )
+    end
+
     context 'success' do
       it 'returns a session token' do
         expect(client.login).to match(/[a-z0-9-]{36}/)
@@ -21,6 +21,22 @@ RSpec.describe Metabase::Endpoint::Session do
     context 'incorrect username or password' do
       it 'raises error' do
         expect { incorrect_password.login }.to raise_error(Metabase::BadRequest)
+      end
+    end
+  end
+
+  describe 'logout', vcr: true do
+    context 'success' do
+      include_context 'login'
+
+      it 'returns true' do
+        expect(client.logout).to be(true)
+      end
+    end
+
+    context 'not logged in' do
+      it 'raises error' do
+        expect { client.logout }.to raise_error(Metabase::BadRequest)
       end
     end
   end
