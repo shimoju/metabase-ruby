74ed45d5a1fcea8cb73663fdd5b37f864b7d656b
Kohei Morita <mrtc0@ssrf.in>
2020-12-25 16:44:54 +0900

    add spec

---
+++ spec/metabase/endpoint/dataset_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+RSpec.describe Metabase::Endpoint::Dataset do
+  include_context 'login'
+
+  let(:query) {
+    '{"type":"native","native":{"query":"SELECT * FROM orders LIMIT 1;"},"database":1}'
+  }
+
+  describe 'query_dataset', vcr: true do
+    context 'success' do
+      it 'returns query results of the dataset' do
+        result = client.query_dataset(query: query)
+        expect(result).to be_kind_of(Array)
+      end
+    end
+
+    context 'specify format' do
+      it 'returns query results of the dataset as specified format' do
+        result = client.query_dataset(query: query, format: 'csv')
+        expect(result).to be_kind_of(String)
+      end
+    end
+  end
+end
