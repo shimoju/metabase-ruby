2c807ee5240fed2b9c45a7671fada8193baa6e18
William Pavei Antero <williamantero@gmail.com>
2021-04-21 12:29:29 -0300

    Add ability to fetch a single dashboard and create a new dashboard

--- lib/metabase/endpoint/dashboard.rb
+++ lib/metabase/endpoint/dashboard.rb
@@ -11,6 +11,25 @@ module Metabase
       def dashboards(**params)
         get('/api/dashboard', **params)
       end
+
+      # Fetch the dashboard.
+      #
+      # @param dashboard_id [Integer, String] Dashboard ID
+      # @param params [Hash] Query string
+      # @return [Hash] Parsed response JSON
+      # @see https://github.com/metabase/metabase/blob/master/docs/api-documentation.md#get-apidashboardid
+      def dashboard(dashboard_id, **params)
+        get("/api/dashboard/#{dashboard_id}", **params)
+      end      
+
+      # Create a new dashboard.
+      #
+      # @param params [Hash] Query string
+      # @return [Hash] Parsed response JSON
+      # @see https://github.com/metabase/metabase/blob/master/docs/api-documentation.md#post-apidashboard
+      def new_dashboard(**params)        
+        post('/api/dashboard', **params)
+      end
     end
   end
 end
--- spec/metabase/endpoint/dashboard_spec.rb
+++ spec/metabase/endpoint/dashboard_spec.rb
@@ -2,12 +2,32 @@
 
 RSpec.describe Metabase::Endpoint::Dashboard do
   include_context 'login'
+  let(:dashboard_id) { 1 }
 
   describe 'dashboards', vcr: true do
     context 'success' do
       it 'returns all dashboards' do
         dashboards = client.dashboards
         expect(dashboards).to be_kind_of(Array)
+      end
+    end
+  end
+
+  describe 'dashboard', vcr: true do
+    context 'success' do
+      it 'returns the dashboard' do
+        dashboard = client.dashboard(dashboard_id)
+        expect(dashboard).to be_kind_of(Hash)
+      end
+    end
+  end
+
+  describe 'new_dashboard', vcr: true do
+    context 'success' do
+      it 'create a new dashboard' do
+        dashboard = client.dashboard(dashboard_id)
+        new_dashboard = client.new_dashboard(**dashboard)
+        expect(dashboard).to be_kind_of(Hash)
       end
     end
   end
