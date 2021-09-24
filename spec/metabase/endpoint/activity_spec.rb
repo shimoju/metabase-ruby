33a7abbd757f977118f209d34d2fb4ffd3a0bca5
THIVEND <baptiste.thivend@protonmail.com>
2021-09-24 17:14:16 +0200

    Add /api/activity/recent_views endpoint

--- spec/metabase/endpoint/activity_spec.rb
+++ spec/metabase/endpoint/activity_spec.rb
@@ -6,8 +6,20 @@ RSpec.describe Metabase::Endpoint::Activity do
   describe 'activities', vcr: true do
     context 'success' do
       it 'returns recent activities' do
-        activities = client.activities
+        activities = client.activity
         expect(activities).to be_kind_of(Array)
+      end
+    end
+  end
+
+  describe 'activity_recent_views', vcr: true do
+    context 'success' do
+      it 'returns the list of 10 things the current user has been viewing most recently' do
+        byebug
+        all_activities = client.activity
+        recent_activities = client.activity(recent_views: true)
+        expect(recent_activities).to be_kind_of(Array)
+        expect(all_activities.size).to be > recent_activities.size
       end
     end
   end
