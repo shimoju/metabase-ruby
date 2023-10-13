38e92434304b952fa6de1047e0725934fb3c76e8
Matthew Gutierrez <matthewgutierrez@Fayleighas-MacBook-Air.local>
2023-10-13 00:41:43 -0500

    Setup initial test stub

---
+++ spec/webmock/user_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.describe Metabase::Endpoint::User do
+  # include_context 'login'
+
+  context 'success' do
+    it 'Fetches Current User' do
+      # Stub a GET request to retrieve data for the current user
+      stub_request(:get, 'https://metabase.com/api/user/current')
+        .to_return(status: 200, body: 'Response Data', headers: { 'Content-Type' => 'application/json' })
+
+      # Assert the response and test your code's behavior
+      expect('200').to eq('200')
+      expect('Response Data').to eq('Response Data')
+    end
+  end
+end
