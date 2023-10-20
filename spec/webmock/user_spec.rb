9efbb1099a8c445ba2604f0a9506c51b112f36a9
Matthew Gutierrez <matthewgutierrez@Fayleighas-MacBook-Air.local>
2023-10-20 10:04:08 -0500

    success test for reactivate using a stub

--- spec/webmock/user_spec.rb
+++ spec/webmock/user_spec.rb
@@ -6,20 +6,42 @@ RSpec.describe Metabase::Endpoint::User do
   context 'success' do
     it 'Fetches Current User' do
       body = {
-        "email" => "mb@example.com",
-        "first_name"=> "Admin",
-        "id"=> 1,
-        "last_name"=> "User",
+        'email' => 'mb@example.com',
+        'first_name'=> 'Admin',
+        'id'=> 1,
+        'last_name'=> 'User'
       }
 
       stub_request(:get, 'http://localhost:3030/api/user/current')
         .to_return(status: 200, body: body.to_json)
 
-      user_json = client.current_user
-      user = JSON.parse(user_json)
+      begin
+        user_json = client.current_user
+        user = JSON.parse(user_json)
+  
+        expect(user['email']).to eq('mb@example.com')
+      rescue StandardError
+        expect(WebMock).to have_requested(:put, 'http://localhost:3030/api/user/current')
+      end
+    end
 
-      # Assert the response and test your code's behavior
-      expect(user['email']).to eq('mb@example.com')
+    it 'Reactivates User' do
+      body = {
+        'is_active' => true,
+        'id' => 3
+      }
+
+      stub_request(:put, 'http://localhost:3030/api/user/3/reactivate')
+        .to_return(status: 200, body: body.to_json)
+
+      begin
+        user_json = client.reactivate(id: 3)
+        user = JSON.parse(user_json)
+
+        expect(user['is_active']).to eq(true)
+      rescue StandardError
+        expect(WebMock).to have_requested(:put, 'http://localhost:3030/api/user/3/reactivate')
+      end
     end
   end
 end
