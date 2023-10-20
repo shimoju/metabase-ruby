2709af4e28d5bb4f4dcc9db048848cec2d072568
Matthew Gutierrez <matthewgutierrez@Fayleighas-MacBook-Air.local>
2023-10-20 12:16:53 -0500

    reactive stub failure

--- spec/webmock/user_spec.rb
+++ spec/webmock/user_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe Metabase::Endpoint::User do
   include_context 'login'
 
+  let(:host) { 'localhost:3030' }
+
   context 'success' do
     it 'Fetches Current User' do
       body = {
@@ -12,16 +14,16 @@ RSpec.describe Metabase::Endpoint::User do
         'last_name'=> 'User'
       }
 
-      stub_request(:get, 'http://localhost:3030/api/user/current')
+      stub_request(:get, "#{host}/api/user/current")
         .to_return(status: 200, body: body.to_json)
 
       begin
         user_json = client.current_user
         user = JSON.parse(user_json)
-  
+
         expect(user['email']).to eq('mb@example.com')
       rescue StandardError
-        expect(WebMock).to have_requested(:put, 'http://localhost:3030/api/user/current')
+        expect(WebMock).to have_requested(:put, "#{host}/api/user/current")
       end
     end
 
@@ -31,7 +33,7 @@ RSpec.describe Metabase::Endpoint::User do
         'id' => 3
       }
 
-      stub_request(:put, 'http://localhost:3030/api/user/3/reactivate')
+      stub_request(:put, "#{host}/api/user/3/reactivate")
         .to_return(status: 200, body: body.to_json)
 
       begin
@@ -40,8 +42,21 @@ RSpec.describe Metabase::Endpoint::User do
 
         expect(user['is_active']).to eq(true)
       rescue StandardError
-        expect(WebMock).to have_requested(:put, 'http://localhost:3030/api/user/3/reactivate')
+        expect(WebMock).to have_requested(:put, "#{host}/api/user/3/reactivate")
       end
+    end
+  end
+
+  context 'user already exists to reactivate' do
+    before do
+      stub_request(:put, "#{host}/api/user/3/reactivate")
+        .to_return(status: 400, body: 'BadRequest')
+    end
+
+    it 'should return error' do
+      message = %r{^PUT http://#{host}/api/user/3/reactivate: 400 - BadRequest}
+      expect { client.reactivate(id: 3) }
+        .to raise_error(Metabase::BadRequest, message)
     end
   end
 end
